bd4ad641ff046d7649f5c04d6ad69928d3d666dc
Jordan Hudgens <jordan@devcamp.com>
2023-02-01 13:22:24 -0700

    Created starter version of code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,52 +2,12 @@ import * as React from "react";
 import { hipsterPhrases, techPhrases } from "./data";
 import Layout from "./Layout";
 
-function generateData(numberOfItems = 15) {
-  const data: { id: string; title: string; summary: string }[] = [];
-
-  for (let i = 0; i < numberOfItems; i++) {
-    data.push({
-      id: (Math.random() * 100000).toString(),
-      title: techPhrases[Math.floor(Math.random() * techPhrases.length)],
-      summary:
-        hipsterPhrases[Math.floor(Math.random() * hipsterPhrases.length)],
-    });
-  }
-
-  return data;
-}
-
 type DataType = { id: string; title: string; summary: string };
 
 export default function Home() {
   const [data, setData] = React.useState<DataType[]>([]);
   const observer = React.useRef(null);
   const lastElementRef = React.useRef(null);
-
-  React.useEffect(() => {
-    setData(generateData());
-  }, []);
-
-  const fetchData = async () => {
-    const newData = await generateData(20);
-    setData((prevData) => [...prevData, ...newData]);
-  };
-
-  React.useEffect(() => {
-    if (observer.current) {
-      observer.current.disconnect();
-    }
-
-    observer.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting && lastElementRef.current) {
-        fetchData();
-      }
-    });
-
-    if (lastElementRef.current) {
-      observer.current.observe(lastElementRef.current);
-    }
-  }, [data]);
 
   return (
     <Layout>
